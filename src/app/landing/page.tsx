41bc8f59e0ac3716a61d70c612a6e359ccab368f
Serg032 <sergioradigales@gmail.com>
2024-03-05 21:04:02 +0100

    wip

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,23 @@
+import { Box, Typography, Container, Button } from "@mui/material";
+import Link from "next/link";
+
+const LandingPage = () => {
+  return (
+    <Box className="h-screen w-screen flex flex-col gap-10 items-center">
+      <Box className="w-screen flex flex-col items-center gap-4 p-3">
+        <Typography variant="h3">Get Fit</Typography>
+        <Typography variant="h4">Your new challenge</Typography>
+      </Box>
+      <Box className="flex flex-col gap-3">
+        <Button variant="contained" color="primary">
+          <Link href={"/signin"}>Sign up</Link>
+        </Button>
+        <Button variant="contained" color="primary">
+          Sign In
+        </Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default LandingPage;

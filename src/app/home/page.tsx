991ef4d26a336d52fdb8669fd36fb1ac2a4c1cf3
Serg032 <sergioradigales@gmail.com>
2024-03-10 01:09:32 +0100

    auth working

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,29 @@
+"use client";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
+import { authenticateUser } from "../auth/authenticate-user";
+import { useRouter } from "next/navigation";
 
 const HomePage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      router.push("signin");
+      return;
+    }
+    async function auth() {
+      const response = await authenticateUser(token!);
+      return response;
+    }
+    auth().then((response) => {
+      if (!response.tokenData) {
+        router.push("signin");
+      }
+    });
+  }, [router]);
+
   return <Box>home</Box>;
 };
 
